8e21a6fb4cc1f02cd61106808d32756dc4844c0d
lovapota <rfenosoa1@gmail.com>
2022-05-07 17:08:46 -0700

    ajout de javascript

--- assets/Js/home.js
+++ assets/Js/home.js
@@ -20,8 +20,107 @@ let image = ["assets/Illustrations/Home/Batslider2.png",
             }
         }
     setInterval('changeImg()', 3000);
+//Action du boutton confirm
+const confirmation = document.getElementsByClassName('confirm');
+const contact = document.getElementsByClassName('contact');
+const promptM = document.getElementById('prompt');
+const one = document.getElementById('one');
+const two = document.getElementById('two');
 
-//Slide avec des bouttons next et previous
+confirmation[0].addEventListener('click', () => {
+    contact[0].style.display = 'none';
+    one.style.display = 'none';
+    two.style.display = 'none';
+    promptM.style.display = 'block';
+});
+    //Action annulation du confirm
+    promptM.addEventListener('click', () => {
+        contact[0].style.display = 'block';
+        one.style.display = 'flex';
+        two.style.display = 'flex';
+        promptM.style.display = 'none';
+});
+//Animation Citation
+const dataCitation = [{
+    citationB : `Soit on meurt en héros, soit on vit assez longtemps pour se voir endosser le rôle du méchant.`,
+    auteurB : `The Dark Knight : Le Chevalier Noir `,
+    dateB : `Harvey Dent (2008)`,
+    },
+    {
+    citationB : `Le héros peut être en chacun, même en celui qui fait une chose aussi simple et rassurante que mettre un manteau sur les épaules d'un garçon et ainsi lui faire comprendre que le monde ne s'est pas écroulé.`,
+    auteurB : `The Dark Knight Rises`,
+    dateB : `Batman à James Gordon (2012)`,
+    },
+    {
+    citationB : `Ah ! Tu crois que l'obscurité est ton allié. Tu n'a fait qu'adopter la pénombre. Moi je suis né dedans, j'ai été façonné par elle. Je n'ai découvert la lumière qu'a l'âge d'homme, et alors, elle m'a juste paru AVEUGLANTE !`,
+    auteurB : `The Dark Knight Rises `,
+    dateB : `Bane à Batman (2012)`,
+    },
+    {
+    citationB : `- Je vous prépare la batmoto monsieur ?
+    - En pleine journée Alfred ? Pas très subtile.
+    - La Lamborghini alors... beaucoup plus subtile.
+    `,
+    auteurB : `The Dark Knight: Le Chevalier Noir `,
+    dateB : `Alfred et Batman (2008)`,
+    },
+    {
+    citationB : `Freeze, t'es givré !`,
+    auteurB : `Batman & Robin `,
+    dateB : `Batman à Mr Freeze (1997)`,
+    },
+    {
+    citationB : `La peur est un outil. Cette lumière qui déchire le ciel, ce n’est pas qu’un appel à l’aide. C’est un avertissement.`,
+    auteurB : `Batman`,
+    dateB : `Batman (2022)`,
+    },
+    {
+    citationB : `- Pourquoi tombons nous Bruce ?
+    - …
+    - Pour mieux apprendre à nous relever.
+    `,
+    auteurB : `Batman Begins `,
+    dateB : `Thomas Wayne à Bruce Wayne 8 ans (2005)`,
+    },
+    {
+    citationB : `Des hommes tombent du ciel, les dieux crachent la foudre, des innoncents meurent. C’est comme cela que ça commence, monsieur. De la fureur, de la fièvre, un sentiment d’impuissance, qui rend les hommes bien… cruels.`,
+    auteurB : `Batman v Superman : l’aube de la justice `,
+    dateB : `Alfred à Batman (2016)`,
+    },
+    {
+    citationB : `La seule façon raisonnable de vivre en ce bas monde, c'est en dehors des règles.`,
+    auteurB : `The Dark Knight : Le Chevalier Noir`,
+    dateB : `Le Joker à Batman (2008)`,
+    },]
+    //Variable des citation
+    const cit = document.getElementById('citation');
+    const aut = document.getElementById('auteur');
+    const dat = document.getElementById('date');
+    const rondB = document.getElementsByClassName('rond');
+    console.log(rondB)
+    let r = 0;
+    function slideCitation() {
+        cit.textContent = `${dataCitation[r].citationB}`;
+        aut.textContent = `${dataCitation[r].auteurB}`;
+        dat.textContent = `${dataCitation[r].dateB}`;
+        if (r < dataCitation.length-1) {
+                r += 1;
+                rondB[r].style.backgroundColor = '#19587D';
+                rondB[r -1].style.backgroundColor = 'transparent';
+                
+        } else {
+                r = 0;
+                rondB[r].style.backgroundColor = '#19587D';
+                rondB[dataCitation.length - 1].style.backgroundColor = 'transparent';
+        }
+    }
+    setInterval('slideCitation()', 3000);
+
+
+//Special web
+let WidthOfEcran =  window.innerWidth;
+if( WidthOfEcran >=768 ){
+//Slide avec des bouttons next et previous pour web
 let data = [{//Donne a entre dans HTML
     titre : 'The Dark Knigth',
     num : '9',
@@ -43,13 +142,13 @@ let data = [{//Donne a entre dans HTML
     lien :`https://www.youtube.com/embed/jXrFsn9pcZY`,
     image :`assets/Illustrations/Home/Bathome8.png`,
 },]
-//Variable a utilise
+//Variable a utilise pour web
 const titreS = document.getElementById('titre');
 const numS = document.getElementById('num');
 const texS = document.getElementById('textSlide');
 const srcS = document.getElementById('iframe');
 const sary = document.getElementById('saryS');
-//Variable boutton
+//Variable boutton pour web
 const btnNext = document.getElementById('next');
 const btnPrevious = document.getElementById('previous');
 let j = -1
@@ -101,95 +200,43 @@ for(let b = 0; b < fig.length; b++){
         figcaption[b].style.display = 'none';
     })
 };
-
-//Action du boutton confirm
-const confirmation = document.getElementsByClassName('confirm');
-const contact = document.getElementsByClassName('contact');
-const promptM = document.getElementById('prompt');
-const one = document.getElementById('one');
-const two = document.getElementById('two');
-
-confirmation[0].addEventListener('click', () => {
-    contact[0].style.display = 'none';
-    one.style.display = 'none';
-    two.style.display = 'none';
-    promptM.style.display = 'block';
-});
-//Action annulation du confirm
-promptM.addEventListener('click', () => {
-    contact[0].style.display = 'block';
-    one.style.display = 'blcok';
-    two.style.display = 'blcok';
-    promptM.style.display = 'none';
-});
-
-
-
-//Animation Citation
-const dataCitation = [{
-citation : `Soit on meurt en héros, soit on vit assez longtemps pour se voir endosser le rôle du méchant.`,
-auteur : `The Dark Knight : Le Chevalier Noir `,
-date : `Harvey Dent (2008)`,
-},
-{
-citation : `Le héros peut être en chacun, même en celui qui fait une chose aussi simple et rassurante que mettre un manteau sur les épaules d'un garçon et ainsi lui faire comprendre que le monde ne s'est pas écroulé.`,
-auteur : `The Dark Knight Rises`,
-date : `Batman à James Gordon (2012)`,
-},
-{
-citation : `Ah ! Tu crois que l'obscurité est ton allié. Tu n'a fait qu'adopter la pénombre. Moi je suis né dedans, j'ai été façonné par elle. Je n'ai découvert la lumière qu'a l'âge d'homme, et alors, elle m'a juste paru AVEUGLANTE !`,
-auteur : `The Dark Knight Rises `,
-date : `Bane à Batman (2012)`,
-},
-{
-citation : `- Je vous prépare la batmoto monsieur ?
-- En pleine journée Alfred ? Pas très subtile.
-- La Lamborghini alors... beaucoup plus subtile.
-`,
-auteur : `The Dark Knight: Le Chevalier Noir `,
-date : `Alfred et Batman (2008)`,
-},
-{
-citation : `Freeze, t'es givré !`,
-auteur : `Batman & Robin `,
-date : `Batman à Mr Freeze (1997)`,
-},
-{
-citation : `La peur est un outil. Cette lumière qui déchire le ciel, ce n’est pas qu’un appel à l’aide. C’est un avertissement.`,
-auteur : `Batman`,
-date : `Batman (2022)`,
-},
-{
-citation : `- Pourquoi tombons nous Bruce ?
-- …
-- Pour mieux apprendre à nous relever.
-`,
-auteur : `Batman Begins `,
-date : `Thomas Wayne à Bruce Wayne 8 ans (2005)`,
-},
-{
-citation : `Des hommes tombent du ciel, les dieux crachent la foudre, des innoncents meurent. C’est comme cela que ça commence, monsieur. De la fureur, de la fièvre, un sentiment d’impuissance, qui rend les hommes bien… cruels.`,
-auteur : `Batman v Superman : l’aube de la justice `,
-date : `Alfred à Batman (2016)`,
-},
-{
-citation : `La seule façon raisonnable de vivre en ce bas monde, c'est en dehors des règles.`,
-auteur : `The Dark Knight : Le Chevalier Noir`,
-date : `Le Joker à Batman (2008)`,
-},]
-//Variable des citation
-const cit = document.getElementById('citation');
-const aut = document.getElementById('auteur');
-const dat = document.getElementById('date');
-let r = 0;
-function slideCitation() {
-    cit.textContent = `${dataCitation[r].citation}`;
-    aut.textContent = `${dataCitation[r].auteur}`;
-    dat.textContent = `${dataCitation[r].date}`;
-        if (r < dataCitation.length) {
-            r += 1
-        } else {
-            r = 0
+}else{
+    dataBat = [{
+        imgBat : `assets/Illustrations/Home/Bathome2.png`,
+        nameBat : `Robert Pattinson`,
+        date : `2022`,
+        ImgAc : `assets/Illustrations/Home/Robert_Pattinson.png`,
+    },
+    {
+        imgBat : `assets/Illustrations/Home/Bathome3.png`,
+        nameBat : `Christian Bale`,
+        date : `2006-2008-2012`,
+        ImgAc : `assets/Illustrations/Home/Christian_Bale.png`,
+    },
+    {
+        imgBat : `assets/Illustrations/Home/Bathome4.png`,
+        nameBat : `Ben Affleck`,
+        date : `2016-2017/2020-2023`,
+        ImgAc : `assets/Illustrations/Home/Ben_Affleck.png`,
+    }]
+     //Les acteur de Batman
+    const imgBa = document.querySelector('#bat1>img');
+    const nomBa = document.querySelector('figcaption>p');
+    const dateB = document.querySelector('figcaption .yellow');
+    const imgAcB = document.querySelector('figcaption img');
+    console.log(imgBa)
+    let v = 0;
+    function changeHeros(){
+            imgBa.src = `${dataBat[v].imgBat}`;
+            nomBa.textContent = `${dataBat[v].nameBat}`
+            dateB.textContent = `${dataBat[v].date}`
+            imgAcB.src = `${dataBat[v].ImgAc}`;
+            if (v < dataBat.length - 1) {
+                    v += 1;   
+            } else {
+                    v = 0;
+            }
         }
+        setInterval('changeHeros()', 3000);
     }
-setInterval('slideCitation()', 2000);
+
